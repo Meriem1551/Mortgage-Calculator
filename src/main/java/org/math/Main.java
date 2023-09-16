f906cd7cc3e31f932c437e7a26888592ed7a4888
Meriem1551 <meriem200409@gmail.com>
2023-09-16 16:17:10 +0200

    update project

--- src/main/java/org/math/Main.java
+++ src/main/java/org/math/Main.java
@@ -1,7 +1,27 @@
 package org.math;
 
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Principal: ");
+        int Principal = scanner.nextInt();
+        System.out.print("Annual Interest Rate: ");
+        float annualInterestRate = scanner.nextFloat();
+        float monthlyInterestRate = annualInterestRate / PERCENT /MONTHS_IN_YEAR;
+        System.out.print("Period (Years) ");
+        byte period = scanner.nextByte();
+        long num_of_payment = period * MONTHS_IN_YEAR;
+        double result = Principal
+                * (monthlyInterestRate * Math.pow((1 + monthlyInterestRate), num_of_payment)
+                /(Math.pow((1 + monthlyInterestRate), num_of_payment) - 1));
+        String Montgage = NumberFormat.getCurrencyInstance().format(result);
+        System.out.print("Montgage: " + Montgage);
     }
 }
